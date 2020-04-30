8648e0799c7aa121cbe3e837c3b213694ffdcf2c
Calderon Jonathan <64608867+amadeusSeckler@users.noreply.github.com>
2020-04-30 16:35:36 -0300

    Agregado Boton

--- Trabajo Practico 4/Form1.Designer.cs
+++ Trabajo Practico 4/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -105,11 +106,21 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Cantidad de simulaciones";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox2);
@@ -136,6 +147,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
