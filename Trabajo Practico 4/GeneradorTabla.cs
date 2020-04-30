c03f39246b99cb8ed171e0798135516e8f3189ea
Calderon Jonathan <64608867+amadeusSeckler@users.noreply.github.com>
2020-04-30 17:05:21 -0300

    GeneradorAhoraSi

--- Trabajo Practico 4/GeneradorTabla.cs
+++ Trabajo Practico 4/GeneradorTabla.cs
@@ -8,18 +8,32 @@ namespace Trabajo_Practico_4
 {
     class GeneradorTabla
     {
-        public void generadorRandomClima(int cantidad, int bandera) 
+        Random rnd = new Random();
+        public double[] generadorRandomClima(int cantidad, int bandera) 
         {
+            
+
             if (bandera == 1)
             {
-                double[] valores = { 0.72,0.33,0.39,0.86,0.85,0.37,0.3,0.48,0.31,0.97,0.4,0.91,0.73,0.33,0.53,0.55,0.59,0.01,0.57,0.8 };
+                double[] valoresClima = { 0.72,0.33,0.39,0.86,0.85,0.37,0.3,0.48,0.31,0.97,0.4,0.91,0.73,0.33,0.53,0.55,0.59,0.01,0.57,0.8 };
 
+                return valoresClima;
             }
             else 
             {
-                dou
+                double[] valoresClima = new double[cantidad];
+
+                for (int i = 0; i < cantidad; i++)
+                {
+                    valoresClima[i] = rnd.NextDouble();
+                }
+
+                return valoresClima;
             }
 
         }
+
     }
+
+    
 }
