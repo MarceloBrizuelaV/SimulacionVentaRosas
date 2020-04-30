b543d679287830c4c54c0c2f3f92f3b4873b0553
MarceloBrizuelaV <marceloestebanbrizuelavega@gmail.com>
2020-04-30 19:34:32 -0300

    Actividad a y b

--- Trabajo Practico 4/Form1.Designer.cs
+++ Trabajo Practico 4/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(38, 35);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(675, 236);
+            this.dataGridView1.Size = new System.Drawing.Size(586, 236);
             this.dataGridView1.TabIndex = 0;
             // 
             // textBox1
---
+++ Trabajo Practico 4/Solucion1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Practico_4
+{
+    class Solucion1
+    {
+
+
+        public double[] calcular(double[,] matriz, int cantReservada)
+        {
+            double[] columna = new double[matriz.GetLength(0) + 1];
+            double costo = cantReservada * 8;
+            double gananciaAcumulada = 0;
+
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                double demanda = matriz[i, matriz.GetLength(1) - 2];
+                double venta = matriz[i, matriz.GetLength(1) - 1];
+                if (demanda < cantReservada)
+                {
+                    columna[i] = (venta + (cantReservada - demanda) * 1.2) - costo;
+                }
+                else
+                {
+                    columna[i] = (venta - (demanda - cantReservada) * 12) - costo;
+                }
+
+                gananciaAcumulada += columna[i];
+            }
+
+            columna[columna.Length] = gananciaAcumulada;
+            return columna;
+        }
+
+
+        public double calcularPromedio(double[] col) 
+        {
+            return col[col.Length - 1] / col.Length - 1;
+        }
+
+    }
+}
