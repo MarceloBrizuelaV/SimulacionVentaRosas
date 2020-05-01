29d71336f3b6fdd4a525b34e92b35fc39c2ea247
MarceloBrizuelaV <marceloestebanbrizuelavega@gmail.com>
2020-05-01 06:11:53 -0300

    Avanzando

--- Trabajo Practico 4/Form1.cs
+++ Trabajo Practico 4/Form1.cs
@@ -38,7 +38,7 @@ namespace Trabajo_Practico_4
                 dataTablaBase.Rows[i].Cells[3].Value = tabla[i, 3].ToString();
             }
 
-            double[] ganancias = calc.calcular(tabla, Convert.ToInt32(txtReserva.Text), cbDiaAnterior.Checked);
+            double[] ganancias = calc.calcular(tabla, Convert.ToInt32(txtReserva.Text), 12, 8, 11, cbDiaAnterior.Checked, cbPuedeComprar.Checked);
             txtPromedioGanancia.Text = Convert.ToString(calc.calcularPromedio(ganancias));
  
 
--- Trabajo Practico 4/Solucion1.cs
+++ Trabajo Practico 4/Solucion1.cs
@@ -10,16 +10,69 @@ namespace Trabajo_Practico_4
     {
 
 
-        public double[] calcular(double[,] matriz, int cantReservada, Boolean anterior)
+        public double[] calcular(double[,] matriz, int cantReservada, double precioVenta, double precioCompra, double precioCompraFaltante, Boolean diaAnterior, Boolean puedeComprar)
         {
             double[] columna = new double[matriz.GetLength(0) + 1];
-            
             double gananciaAcumulada = 0;
-            if (anterior)
+
+            //Resolucion del inciso D, aplicando a los incisos A, B y C
+            //En este caso puede comprar a otro precio en caso de faltarle
+            if (puedeComprar)
+            {
+                //En caso de utilizar la demanda del dia anterior para comprar
+                if (diaAnterior)
+                {
+                    for (int i = 0; i < matriz.GetLength(0); i++)
+                    {
+                        double costo = cantReservada * precioCompra;
+                        double demanda = matriz[i, matriz.GetLength(1) - 2];
+                        double venta = matriz[i, matriz.GetLength(1) - 1];
+                        if (demanda <= cantReservada)
+                        {
+                            columna[i] = (venta + (cantReservada - demanda) * 1.2) - costo;
+                        }
+                        else
+                        {
+                            columna[i] = (venta - costo - (demanda - cantReservada) * precioCompraFaltante);
+                        }
+                        cantReservada = Convert.ToInt32(demanda);
+                        gananciaAcumulada += columna[i];
+                    }
+                    columna[columna.Length - 1] = gananciaAcumulada;
+                    return columna;
+                }
+
+                //En caso de no utilizar la demanda del dia anterior (compra fija)
+                else
+                {
+                    double costo = cantReservada * precioCompra;
+                    for (int i = 0; i < matriz.GetLength(0); i++)
+                    {
+                        double demanda = matriz[i, matriz.GetLength(1) - 2];
+                        double venta = matriz[i, matriz.GetLength(1) - 1];
+                        if (demanda <= cantReservada)
+                        {
+                            columna[i] = (venta + (cantReservada - demanda) * 1.2) - costo;
+                        }
+                        else
+                        {
+                            columna[i] = (venta - (demanda - cantReservada) * precioCompraFaltante) - costo;
+                        }
+                        gananciaAcumulada += columna[i];
+                    }
+                    columna[columna.Length - 1] = gananciaAcumulada;
+                    return columna;
+                }
+            }
+            
+
+            //Resolucion de los incisos A, B y C
+            //En este caso no puede comprar cuando le faltan
+            if (diaAnterior)
             {
                 for (int i = 0; i < matriz.GetLength(0); i++)
                 {
-                    double costo = cantReservada * 8;
+                    double costo = cantReservada * precioCompra;
                     double demanda = matriz[i, matriz.GetLength(1) - 2];
                     double venta = matriz[i, matriz.GetLength(1) - 1];
                     if (demanda < cantReservada)
@@ -28,17 +81,17 @@ namespace Trabajo_Practico_4
                     }
                     else
                     {
-                        columna[i] = (venta - (demanda - cantReservada) * 12) - costo;
+                        columna[i] = (venta - (demanda - cantReservada) * precioVenta) - costo;
                     }
-
                     cantReservada = Convert.ToInt32(demanda);
                     gananciaAcumulada += columna[i];
-                    
-                }
+                    }
+                columna[columna.Length - 1] = gananciaAcumulada;
+                return columna;
             }
             else
             {
-                double costo = cantReservada * 8;
+                double costo = cantReservada * precioCompra;
                 for (int i = 0; i < matriz.GetLength(0); i++)
                 {
                     double demanda = matriz[i, matriz.GetLength(1) - 2];
@@ -49,14 +102,14 @@ namespace Trabajo_Practico_4
                     }
                     else
                     {
-                        columna[i] = (venta - (demanda - cantReservada) * 12) - costo;
+                        columna[i] = (venta - (demanda - cantReservada) * precioVenta) - costo;
                     }
 
                     gananciaAcumulada += columna[i];
                 }
+                columna[columna.Length - 1] = gananciaAcumulada;
+                return columna;
             }
-            columna[columna.Length - 1] = gananciaAcumulada;
-            return columna;
         }
 
         
