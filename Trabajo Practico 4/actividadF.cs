3fc884b245140aa20f4be3fe87c80e0ce1796b8e
guadyrukia <guadalupe.miranda@ymail.com>
2020-04-30 22:32:54 -0300

    Actividad F lista

--- Trabajo Practico 4/actividadF.cs
+++ Trabajo Practico 4/actividadF.cs
@@ -8,7 +8,7 @@ namespace Trabajo_Practico_4
 {
     class actividadF
     {
-        public double[,] puntoA(int cantidad)
+        public double[] puntoA(int cantidad, int reserva, Boolean check)
         
         {
             GeneradorTabla tabla = new GeneradorTabla();
@@ -17,31 +17,58 @@ namespace Trabajo_Practico_4
             double[,] matriz = new double[cantidad,4];
             matriz = tabla.tablaBase(cantidad, 1);
 
+            double perdida = 1.20;
             
 
-            double perdida = 1.20;
-            double gastado = 64;
-
             //Realizo una matriz con los datos de ganancia y los dias.
-            double[,] minimatriz = new double[cantidad + 1, 2];
+            double[] minimatriz = new double[cantidad + 1];
 
-            for (int i = 0; i < cantidad; i++)
+            if (check)
             {
-                double demanda = matriz[i, 2];
-                double precio = matriz[i, 3];
 
-                if (demanda > 8)
+                for (int i = 0; i < cantidad; i++)
                 {
-                    
-                    minimatriz[i, 0] = ((precio - (demanda - 8) * 12 )-(demanda - 8)* perdida)- gastado;
-                }
+                    double gastado = reserva * 8;
+                    double demanda = matriz[i, 2];
+                    double precio = matriz[i, 3];
 
-                else
-                {
-                    minimatriz[i, 0] = ((precio) - (demanda - 8) * perdida) - gastado;
+                    if (demanda > reserva)
+                    {
+                        minimatriz[i] = ((precio - (demanda - reserva) * 12) - (demanda - reserva) * perdida) - gastado;
+                    }
+
+                    else
+                    {
+                        minimatriz[i] = ((precio) - (reserva - demanda) * perdida) - gastado;
+                    }
+
+                    minimatriz[minimatriz.GetLength(0) - 1] += minimatriz[i];
+                    reserva = Convert.ToInt32(demanda);
                 }
-                
             }
+            else
+            {
+                double gastado = reserva * 8;
+                for (int i = 0; i < cantidad; i++)
+                {
+                    double demanda = matriz[i, 2];
+                    double precio = matriz[i, 3];
+
+                    if (demanda > reserva)
+                    {
+                        minimatriz[i] = ((precio - (demanda - reserva) * 12) - (demanda - reserva) * perdida) - gastado;
+                    }
+
+                    else
+                    {
+                        minimatriz[i] = ((precio) - (reserva - demanda) * perdida) - gastado;
+                    }
+
+                    minimatriz[minimatriz.GetLength(0) - 1] += minimatriz[i];
+                }
+            }
+            
+
 
             return minimatriz;
 
